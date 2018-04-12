bfab41b5dd63c5e41309b49ff0b1b97b7b614593
Samuel <bermanterp@gmail.com>
2018-04-11 20:49:19 -0400

    Name the trip and party members

---
+++ project.py
@@ -0,0 +1,7 @@
+def names():
+	trip = input("Enter a name for your trip: ")
+	party_num = int(input("Enter the amount of people in your party: ")
+	party = []
+	for i in range(party_num):
+		name = input("Enter the name of the next party member: ")
+		party.append(name)
