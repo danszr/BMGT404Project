72712bc36a57c4c67718f184b5779df08184bee3
Samuel <bermanterp@gmail.com>
2018-04-11 21:07:54 -0400

    Changed variable names

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 def names():
 	trip = input("Enter a name for your trip: ")
-	party_num = int(input("Enter the amount of people in your party: "))
-	party = []
-	for i in range(party_num):
-		name = input("Enter the name of the next party member: ")
-		party.append(name)
+	party_amount = int(input("Enter the amount of people in your party: "))
+	party_members = []
+	for i in range(party_amount):
+		member_name = input("Enter the name of the next party member: ")
+		party_members.append(name)
