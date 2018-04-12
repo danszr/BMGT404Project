96298927f61cc649f6b1344ebe1bfd799714c8ac
Samuel <bermanterp@gmail.com>
2018-04-11 21:25:21 -0400

    payment_choice edits

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ def names():
 		party_members.append(name)
 
 def payment_choice():
-	multiple_payers = input("Are there multiple payers on this payment? 'Y' or 'N': ")
-	choice = input("How would you like to split the bill? \n1) Equal split \n2) Dollar split \n3) Percentage split\n")
+	payer_amount = input("How many people are on this payment? ")
+	choice = input("How would you like to split the bill?\n1) Equal split\n2) Dollar split\n3) Percentage split\n")
 
 payment_choice()
