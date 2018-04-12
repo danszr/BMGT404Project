aeb062e8a087e9e39345974385687a75d385bb6a
Dan Selzer <dselzer@optonline.net>
2018-04-11 20:53:03 -0400

    fixed sam's dumb bug

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 def names():
 	trip = input("Enter a name for your trip: ")
-	party_num = int(input("Enter the amount of people in your party: ")
+	party_num = int(input("Enter the amount of people in your party: "))
 	party = []
 	for i in range(party_num):
 		name = input("Enter the name of the next party member: ")
