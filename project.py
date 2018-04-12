11bb9f8cea2ae6c14d1838c75868180eb0a72029
Samuel <bermanterp@gmail.com>
2018-04-11 21:18:03 -0400

    Payment choice

--- project.py
+++ project.py
@@ -5,3 +5,8 @@ def names():
 	for i in range(party_amount):
 		member_name = input("Enter the name of the next party member: ")
 		party_members.append(name)
+
+def payment_choice():
+	multiple_payers = input("Are there multiple payers on this payment? ''Y'' or ''N''")
+	choice = input("How would you like to split the bill? \n_
+			1) Equal split \n 2) Dollar split \n 3) Percentage split")
