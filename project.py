112111b250e958e5aca3879f8d44db86dca46583
Dan Selzer <dselzer@optonline.net>
2018-05-05 14:32:53 -0400

    almost done, stuck at trying to resolve payment chain

--- project.py
+++ project.py
@@ -1,33 +1,32 @@
 import time
+import math
 from datetime import date
 
 def names():
 	trip_name = input("Enter a name for your trip: ")
 	party_number = int(input("Enter the amount of people in your party: "))
 	party_member = {}
-	
-	for i in range(party_amount):
+
+	for i in range(party_number):
 		member_name = input(f"Enter the name of party member #{i+1}: ")
 		party_member[member_name] = 0
-	print(party_member)
+	#print(party_member)
+	payment_options(party_member, party_number)
 
-def payment_options():
+def payment_options(payers, payer_number):
 	payment_list = []
 	#while True:
 	payment_name = input("Enter a name for your payment, or 'quit' to quit: ")
-	#if payment_name.lower() == 'quit':
+	#if payment_name.l/ower() == 'quit':
 		#break
-	
+
 	payment_year = int(input("Enter the year of your payment: "))
 	payment_month = int(input("Enter the month of your payment: "))
 	payment_day = int(input("Enter the day of your payment: "))
 	payment_date = date(payment_year, payment_month, payment_day)
 
-	payers = {}
 	payment_amount = 0.0
-	payer_number = int(input(f"\nHow many people are on the payment? "))
-	for i in range(payer_number):
-		payer_name = input(f"Who is person #{i+1}? ")
+	for payer_name, amount in payers.items():
 		payer_amount = float(input(f"How much did {payer_name} pay? $"))
 		payers[payer_name] = payer_amount
 		payment_amount += payer_amount
@@ -35,44 +34,70 @@ def payment_options():
 	payment_data = [payment_date, payment_name, payment_amount]
 	payment_list.append(payment_data)
 
-	payment_choice = input("\nHow would you like to split the bill?\n1) Equal split\n2) Dollar split\n3) Percentage split\n")
-	if payment_choice == 1:
-		sum = 0
-		payment_per_person = payment_amount/payer_number
-		for name, amount in payers.items():
-			payers[name] = amount - payment_per_person
-			sum += payers[name]
-		print(payers)
+	sum = 0
+	owes = {}
+	payment_per_person = payment_amount/payer_number
+	for name, amount in payers.items():
+		payers[name] = amount - payment_per_person
+		sum += payers[name]
 
-	#split(payment_amount, payer_number, payer_name, payers)
+		for giver_name, giver_amount in payers.items():
+			if payers[giver_name] < 0:
+				owes[giver_name] = {}
+				for receiver_name, receiver_amount in payers.items():
+					if payers[receiver_name] > 0:
+						if math.fabs(payers[giver_name]) == payers[receiver_name]:
+							owes[giver_name][receiver_name] = payers[receiver_name]
+							payers.remove(giver_name)
+							payers.remove(receiver_name)
+						if math.fabs(payers[giver_name]) > payers[receiver_name]:
+							owes[giver_name][receiver_name] = payers[receiver_name]
+							payers[giver_name] += payers[receiver_name]
+							payers[receiver_name] = 0
+							payers.remove(receiver_name)
+							if payers[giver_name] == 0:
+								payers.remove(giver_name)
+						else:
+							owes[giver_name][receiver_name] = -1*payers[giver_name]
+							payers[receiver_name] += payers[giver_name]
+							payers[giver_name] = 0
+							if payers[receiver_name] == 0:
+								payers.remove(receiver_name)
+	paymentList = []
+	finalPaymentList = []
+	for giver, receivers in owes.items():
+		for receiver, amount in owes[giver].items():
+			if amount > 0:
+				paymentList.append([giver, amount, receiver])
+	print(f"paymentList \n")
 
-#def split(payment_amount, payer_number, payer_name, payers):
-	#payment_choice = 0
-	#while payment_choice != 1 and payment_choice != 2 and payment_choice != 3:
-	#payment_choice = input("\nHow would you like to split the bill?\n1) Equal split\n2) Dollar split\n3) Percentage split\n")
-	#if payment_choice == 1:
-		#sum = 0
-		#payment_per_person = payment_amount/payer_number
-		#for name, amount in payers.items():
-			#payers[name] = amount - payment_per_person
-			#sum += payers[name]
-		#print(payers)
 
-payment_options()
+	for payment1 in paymentList:
+		for payment2 in paymentList:
+			print(f"payment1: {payment1}, payment2: {payment2}")
+			if payment1[0] == payment2[2]:
+				if payment1[1] <= payment2[1]:
+					payment1[2] = payment2[2]
+					payment2[1] = payment2[1] - payment1[1]
+				else:
+					temp = payment1[2]
+					payment1[2] = payment2[2]
+					payment2[2] = temp
 
-#			for giver_name, giver_amount in payers.items():
-#				while payers[giver_name] < 0:
-#					for receiver_name, receiver_amount in #payers.items():
-#						if payers[receiver_name] = 0:
-#							
-#					
-#
-#		elif payment_choice == 2:
-#			
-#
-#		elif payment_choice == 3:
-#			
-#
-#		else:
-#			print("That is not an option. Please choose a different option.")
-#			continue
+					payment1[1] = payment1[1] - payment2[1]
+			print(f"payment1: {payment1}, payment2: {payment2}\n")
+			
+
+	print(paymentList)
+
+
+
+
+	# for giver, receivers in owes.items():
+	# 	print(f"{giver} owes... ")
+	# 	for receiver, amount in owes[giver].items():
+	# 		if amount > 0:
+	# 			print(f"{receiver}: ${amount}")
+	# 	print()
+	# print(owes)
+names()
