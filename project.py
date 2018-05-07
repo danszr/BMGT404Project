28339b3128f75ce44f4cbaceba4bf611ef4a2063
Dan Selzer <dselzer@optonline.net>
2018-05-07 14:54:33 -0400

    Added try/catch for inputs

--- project.py
+++ project.py
@@ -3,15 +3,21 @@ import math
 import collections
 from datetime import date
 
-def names():
+def party():
 	# trip_name = input("Enter a name for your trip: ")
-	party_number = int(input("Enter the amount of people in your party: "))
+	while True:
+		try:
+			party_number = int(input("Enter the amount of people in your party: "))
+		except:
+			continue
+		else:
+			break
 	party_member = {}
 
 	for i in range(party_number):
 		member_name = input(f"Enter the name of party member #{i+1}: ")
 		party_member[member_name] = 0
-	#print(party_member)
+	print()
 	payment_split(party_member, party_number)
 
 def payment_split(ogPayers, payer_number):
@@ -29,20 +35,19 @@ def payment_split(ogPayers, payer_number):
 
 	payment_amount = 0.0
 	for payer_name, amount in ogPayers.items():
-		payer_amount = float(input(f"How much did {payer_name} pay? $"))
+		while True:
+			try:
+				payer_amount = float(input(f"How much did {payer_name} pay? $"))
+			except:
+				continue
+			else:
+				break
 		ogPayers[payer_name] = payer_amount
 		payment_amount += payer_amount
-
+	print()
 	ogPayers = sorted(ogPayers.items(), key = lambda x: x[1])
 	payers = collections.OrderedDict(ogPayers)
 	payersReversed = collections.OrderedDict(sorted(payers.items(), reverse=True))
-
-	# for (key, value) in ogPayers:
-	# 	payers[key] = value
-
-	#print(payers)
-	# payment_data = [payment_date, payment_name, payment_amount]
-	# payment_list.append(payment_data)
 
 	sum = 0
 	owes = {}
@@ -57,7 +62,7 @@ def payment_split(ogPayers, payer_number):
 				if payers[receiver_name] > 0:
 					if math.fabs(payers[giver_name]) == payers[receiver_name]:
 						owes[giver_name][receiver_name] = payers[receiver_name]
-					if math.fabs(payers[giver_name]) > payers[receiver_name]:
+					elif math.fabs(payers[giver_name]) > payers[receiver_name]:
 						owes[giver_name][receiver_name] = payers[receiver_name]
 						payers[giver_name] += payers[receiver_name]
 						payers[receiver_name] = 0
@@ -66,38 +71,10 @@ def payment_split(ogPayers, payer_number):
 						payers[receiver_name] += payers[giver_name]
 						payers[giver_name] = 0
 
-
-	# paymentList = []
-	# finalPaymentList = []
-	# for giver, receivers in owes.items():
-	# 	for receiver, amount in owes[giver].items():
-	# 		if amount > 0:
-	# 			paymentList.append([giver, amount, receiver])
-	# print(paymentList)
-
-
-	# for payment1 in paymentList:
-	# 	for payment2 in paymentList:
-	# 		print(f"payment1: {payment1}, payment2: {payment2}")
-	# 		if payment1[0] == payment2[2]:
-	# 			if payment1[1] <= payment2[1]:
-	# 				payment1[2] = payment2[2]
-	# 				payment2[1] = payment2[1] - payment1[1]
-	# 			else:
-	# 				temp = payment1[2]
-	# 				payment1[2] = payment2[2]
-	# 				payment2[2] = temp
-	#
-	# 				payment1[1] = payment1[1] - payment2[1]
-	# 		print(f"payment1: {payment1}, payment2: {payment2}\n")
-
-
-
-
 	for giver, receivers in owes.items():
-		print(f"{giver} owes... ")
+		print(f"{giver} owes: ")
 		for receiver, amount in owes[giver].items():
 			if amount > 0:
 				print(f"{receiver}: ${amount}")
 		print()
-names()
+party()
